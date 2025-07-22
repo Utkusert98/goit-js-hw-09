f524943bfb8ed951f3586660d6ebcc938832cc2d
Utku Sert <utkusert.arc@gmail.com>
2025-07-22 13:24:22 +0300

    yeni

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
-
 export default defineConfig(({ command }) => {
   return {
     base: '/goit-js-hw-09/',
